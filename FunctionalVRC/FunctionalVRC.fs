209d8968bafd54187542972ffff0b81b19118258
xKiraiChan <xKiraiChan@gmail.com>
2021-10-10 00:39:37 -0400

    Add anti ui banners

--- FunctionalVRC/FunctionalVRC.fs
+++ FunctionalVRC/FunctionalVRC.fs
@@ -9,20 +9,17 @@ type FunctionalVRC() =
     override _this.OnApplicationStart() = 
         FunctionalEnumerator([
             fun () -> 
-                MelonLogger.Msg("Checking...");
                 match GameObject.Find("UserInterface") with
                  | null -> true :> obj
                  | other -> null
             fun () ->
                 MelonLogger.Msg("Found UI Manager")
-                GameObject.Find("UserInterface").transform
-                    .Find("Canvas_QuickMenu(Clone)")
-                    .GetComponent<Canvas>()
-                    .referencePixelsPerUnit <- 0f
+                OnUIInit.execute()
                 null
         ]).Start()
 
 [<assembly: MelonInfo(typeof<FunctionalVRC>, "FunctionalVRC", "0.1.0", "Kirai Chan", "github.com/xKiraiChan/FunctionalVRC")>] 
 [<assembly: MelonGame("VRChat", "VRChat")>] 
+[<assembly: MelonColor(System.ConsoleColor.DarkMagenta)>]
 do ()
     
---
+++ FunctionalVRC/OnUIInit.fs
@@ -0,0 +1,14 @@
+﻿module OnUIInit
+
+open UnityEngine
+
+let _execute = fun () -> 
+    let ui = GameObject.Find("UserInterface").transform;
+    let qm = ui.Find("Canvas_QuickMenu(Clone)");
+
+    qm.GetComponent<Canvas>().referencePixelsPerUnit <- 0f
+
+    let launch_pad = qm.Find("Container/Window/QMParent/Menu_Dashboard/ScrollRect/Viewport/VerticalLayoutGroup");
+    launch_pad.Find("VRC+_Banners").gameObject.active <- false;
+    launch_pad.Find("Carousel_Banners").gameObject.active <- false;
+let execute = _execute

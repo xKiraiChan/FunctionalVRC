6d8f15f3af2663dbeee1e5fea6d0d4a44128e63f
xKiraiChan <xKiraiChan@gmail.com>
2021-10-11 22:53:30 -0400

    add flat style to full menu

--- FunctionalVRC/Modules/UI.fs
+++ FunctionalVRC/Modules/UI.fs
@@ -4,8 +4,10 @@ open UnityEngine
 
 let _execute = fun (ui: Transform) -> 
     let qm = ui.Find("Canvas_QuickMenu(Clone)");
+    let mc = ui.Find("MenuContent");
 
     qm.GetComponent<Canvas>().referencePixelsPerUnit <- 0f
+    mc.GetComponent<Canvas>().referencePixelsPerUnit <- 0f
 
     let launch_pad = qm.Find("Container/Window/QMParent/Menu_Dashboard/ScrollRect/Viewport/VerticalLayoutGroup");
     launch_pad.Find("VRC+_Banners").gameObject.active <- false;
